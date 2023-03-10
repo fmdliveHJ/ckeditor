e3ad832352f3094488d058aa6347bd242d51234a
hyeonjin <gdalove@naver.com>
2023-03-10 18:01:42 +0900

    초기설정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import Editor from '@ckeditor/ckeditor5-build-classic';
+// import Editor from 'ckeditor5-custom/build/ckeditor';
 
+// import Editor from '@ckeditor5-custom-build/build/ckeditor';
+import { Container, Header, Button } from 'semantic-ui-react';
+import './ckeditor.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Container>
+        <CKEditor
+          editor={Editor}
+          data='<p>Hello from CKEditor 5!</p>'
+          onReady={(editor) => {
+            // You can store the "editor" and use when it is needed.
+            console.log('Editor is ready to use!', editor);
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({ event, editor, data });
+          }}
+          onBlur={(event, editor) => {
+            console.log('Blur.', editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log('Focus.', editor);
+          }}
+        />
+      </Container>
     </div>
   );
 }
